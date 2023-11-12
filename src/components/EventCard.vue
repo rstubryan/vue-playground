5a6509578f4b5a7839323a99a1701296534baccf
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-11-12 13:48:44 +0700

    Add event data and props to EventCard.vue and HomeView.vue

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,8 +1,24 @@
-<script setup></script>
+<script setup>
+defineProps({
+  event: {
+    type: Object,
+    required: true
+  }
+})
+</script>
 
 <template>
-  <div class="text-black">
-    <h1>This is an about page</h1>
+  <div class="container mx-auto text-black">
+    <div class="text-base text-center">
+      <div class="border w-96 py-9 mx-auto border-black">
+        This is the <span class="font-bold">{{ event.title }}</span>
+        <br />
+        on
+        <span class="font-bold"
+          >{{ event.date }} at {{ event.location }} start from {{ event.time }}</span
+        >
+      </div>
+    </div>
   </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,34 @@
 <script setup>
-import EventCard from '../components/EventCard.vue'
+import EventCard from '@/components/EventCard.vue'
+import { ref } from 'vue'
+
+const events = ref([
+  {
+    id: 1,
+    title: 'Vue.js Global Summit',
+    date: 'September 30 - October 1, 2021',
+    location: 'Online',
+    time: '9:00 AM - 1:00 PM'
+  },
+  {
+    id: 2,
+    title: 'Vue.js Amsterdam',
+    date: 'February 10 - 12, 2022',
+    location: 'Amsterdam, Netherlands',
+    time: '9:00 AM - 1:00 PM'
+  },
+  {
+    id: 3,
+    title: 'VueConf US',
+    date: 'March 2 - 4, 2022',
+    location: 'Austin, Texas',
+    time: '9:00 AM - 1:00 PM'
+  }
+])
 </script>
 
 <template>
   <div>
-    <EventCard />
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
